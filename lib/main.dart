edabcfec5254db09ab695a403488ab3e65d41071
Said Lhor <sensaitionallaboratory@gmail.com>
2025-05-21 14:44:15 -0400

    edited the AppBar, removed the demo banner, added a hamburger menu.added a nav bar.

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CST2335 Lab 1',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
+
   }
 }
 
@@ -70,13 +71,16 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("ABCDE"),
+          actions: [
+            OutlinedButton(onPressed: () { }, child:Text("Button 1")),
+            OutlinedButton(onPressed: (){ }, child: Text("Button 2"))]
       ),
+      drawer: Drawer
+        (child:Text("Hi there")
+      ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround  ,
@@ -107,6 +111,12 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem( icon: Icon(Icons.camera), label: 'Camera' ),
+        BottomNavigationBarItem( icon: Icon(Icons.add_call), label: 'Phone'  ),
+      ],
+        onTap: (buttonIndex) {  } ,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
