3412777db53ed3a33e49dcbd9593c52dc1ec56ab
Said Lhor <sensaitionallaboratory@gmail.com>
2025-05-21 14:15:32 -0400

    similar to spaceEvenly except that the space at the top and bottom is only half that of the space between the elements.

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+          mainAxisAlignment: MainAxisAlignment.spaceAround  ,
           children: [
             ElevatedButton(
               onPressed: () {
